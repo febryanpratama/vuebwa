1d7add6ed779735dfe5ed32018d2f984fa13551e
FEBRYANPRATAMA\ASUS <febryancpratama@gmail.com>
2021-10-01 21:50:54 +0700

    Upload Project Vue

---
+++ src/views/Cart.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <HeaderShayna />
+
+    <!-- Breadcrumb Section Begin -->
+    <div class="breacrumb-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-left">
+            <div class="breadcrumb-text product-more">
+              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+              <span>Shopping Cart</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Breadcrumb Section Begin -->
+
+    <CartProduct />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+import carousel from "vue-owl-carousel";
+
+// import carousel from "vue-owl-carousel";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import CartProduct from "@/components/CartProduct.vue";
+
+import FooterShayna from "@/components/FooterShayna.vue";
+export default {
+  name: "Cart",
+  components: {
+    HeaderShayna,
+    CartProduct,
+    FooterShayna,
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroCarousel />
+    <BannerCarousel />
+    <InstaShayna />
+    <LogoPartner />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";\
+// import carousel from "vue-owl-carousel";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroCarousel from "@/components/HeroCarousel.vue";
+import BannerCarousel from "@/components/BannerCarousel.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import LogoPartner from "@/components/LogoPartner.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld,
+    HeaderShayna,
+    // carousel,
+    HeroCarousel,
+    BannerCarousel,
+    InstaShayna,
+    LogoPartner,
+    FooterShayna,
+  },
+};
+</script>
